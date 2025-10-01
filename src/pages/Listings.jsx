400ec06679c3c353f022e0976a0c9ce6899588a2
exitrampDev <devteam@exitramp.co>
2025-10-01 11:03:02 +0500

    listing single view website done

--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { useRecoilValue } from "recoil";
 import { propertiesState } from "../recoil/propertiesAtom";
 import PropertyCard from "../components/PropertyCard";
-
+import ArrowIcon from "../assets/arrowIcon.png";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const Listings = () => {
   const properties = useRecoilValue(propertiesState);
-
+  const handleNonUserClick = () => {
+    const signupBtn = document.querySelector(".signup-btn");
+    if (signupBtn) signupBtn.click();
+  };
   return (
     <>
       <Header />
@@ -24,6 +27,19 @@ const Listings = () => {
       </div>
 
       <PropertyCard />
+      <div className="UpgradeFree_when_ready_wrap">
+            <div className="UpgradeFree_when_ready_container">
+              <h3>
+                <span>Interested?</span> Create a Buyer Profile to Get Started
+              </h3>
+              <p>
+                Create a free buyer profile to submit NDAs, unlock seller documents, and connect with businesses ready to talk.
+              </p>
+              <button className="UpgradeFree_when_ready_btn" onClick={handleNonUserClick}>
+                Create Buyer Profile <img src={ArrowIcon} alt="ArrowIcon" />
+              </button>
+            </div>
+          </div>
       <Footer />
     </>
   );
